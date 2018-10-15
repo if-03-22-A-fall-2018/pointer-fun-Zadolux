052f2e0e4aebef047e344d6dbc84b45b1bad0191
zadolux <zadolux@inscomers.net>
2018-10-15 16:47:24 +0200

    Added change function - final commit?

--- pointerfun.c
+++ pointerfun.c
@@ -11,15 +11,23 @@
 */
 #include <stdio.h>
 
+void change_integers(int int_value, int* int_pointer)
+{
+  int_value = 360;
+  *int_pointer = 42;
+}
+
 void print_integers(int int_value, int* int_pointer)
 {
-  printf("Got an integer value %d and an address to an integer with value %d", int_value, *int_pointer);
+  printf("Got an integer value %d and an address to an integer with value %d\n", int_value, *int_pointer);
 }
 
 int main(int argc, char const *argv[]) {
-  int int_value = 34;
+  int int_value = 1337;
   int* int_pointer = &int_value;
 
+  print_integers(int_value, int_pointer);
+  change_integers(int_value, int_pointer);
   print_integers(int_value, int_pointer);
   return 0;
 }
